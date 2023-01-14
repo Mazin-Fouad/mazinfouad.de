622754a10efb767d1a7e0c52a0797293d56a8c66
Mazin Fouad <m.fouad@live.de>
2023-01-14 14:31:51 +0100

    dynamic slide show imgs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'mazinfouad';
+export class AppComponent implements OnInit {
+   images = ['code.jpg', 'laptop.jpg']
+   currentImage = 0;
+
+   ngOnInit(){
+    this.updateImage();
+   }
+
+   updateImage(){
+    
+    setInterval(() => {
+      this.currentImage ++;
+      this.currentImage = this.currentImage % this.images.length
+    }, 2000);
+   }
 }
